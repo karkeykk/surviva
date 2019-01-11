d0f9b00c696535bcd5f32f9964e68119568d0254
karkeykk <karthiorton@gmail.com>
2019-01-11 23:57:05 +0530

    Sentiment added

--- routes/api.js
+++ routes/api.js
@@ -30,6 +30,9 @@ var instanceOAuth = axios.create({
     baseURL: 'https://www.googleapis.com/oauth2/v2/userinfo'
 });
 
+var instanceSent = axios.create({
+    baseURL: 'https://eastasia.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment'
+});
 /*
 PREDICTHQ.COM
 
@@ -39,60 +42,6 @@ Client secret:
 Access token:
 7XvdQkOUUrShrqq3QrHCYmwfJod5Oy
 */
-
-var emotionScore = "";
-var flg = true;
-let response_handler = function (response) {
-    let body = '';
-    response.on('data', function (d) {
-        body += d;
-    });
-    response.on('end', function () {
-        let body_ = JSON.parse(body);
-        //var body__ = JSON.stringify (body_, null, '  ');
-        console.log(body_.documents[0].score);
-        emotionScore = body_.documents[0].score;
-        flg = false;
-        //console.log(emotin);
-    });
-    response.on('error', function (e) {
-        console.log('Error: ' + e.message);
-    });
-};
-
-function get_sentiments(documents) {
-    return new Promise((resolve, reject) => {
-        let body = JSON.stringify(documents);
-        let request_params = {
-            method: 'POST',
-            hostname: 'eastus.api.cognitive.microsoft.com',
-            path: '/text/analytics/v2.0/sentiment',
-            headers: {
-                'Ocp-Apim-Subscription-Key':'840cc8c3afcc4edb9e6910b408eff236'
-            }
-        };
-        let req = https.request(request_params, response_handler);
-        req.write(body);
-        req.end();
-        resolve(req);
-    });
-}
-
-function get_sentiment(documents) {
-    let body = JSON.stringify(documents);
-    let request_params = {
-        method: 'POST',
-        hostname: 'eastus.api.cognitive.microsoft.com',
-        path: '/text/analytics/v2.0/sentiment',
-        headers: {
-            'Ocp-Apim-Subscription-Key': '840cc8c3afcc4edb9e6910b408eff236'
-        }
-    };
-    let req = https.request(request_params, response_handler);
-    req.write(body);
-    req.end();
-    return (req);
-}
 
 
 //**********************HELP**********************
@@ -140,47 +89,31 @@ router.post('/addHelper', function (req, res, next) {
     }).catch(next);
 });
 
-router.post('/addHelp', function (req, res, next) {
+router.post('/addHelp',async function(req,res,next){
     let documents = {
-        'documents': [
-            { 'id': '1', 'language': 'en', 'text': req.body.probDesc }
-        ]
-    }
+        'documents': [{
+            'id': '1',
+            'language': 'en',
+            'text': req.body.probDesc 
+        }]
+    };
+    var emotionScore = "";
     var currentTime = Date();
     currentTime = currentTime.toString().slice(4,24);
-    
-    get_sentiments(documents).then(function () {
-        console.log("hello");
+
+    instanceDisaster.defaults.headers.common['Ocp-Apim-Subscription-Key'] = '810e478db8d14548aa32f228c027725a';
+
+    await instanceSent.post('/',documents).then(function(response){
+        //console.log(response.data.documents[0].score);
+        emotionScore = response.data.documents[0].score;
+        //res.status(200).send(response.data.documents[0].score.toString());
+    }).catch(function(err){
+        if(err.response){
+            console.log(err.response.data);
+            res.send("Unable to retrieve sentiments");
+        }
     });
-    setTimeout(function () {
-        Victim.create({
-            probTitle: req.body.probTitle,
-            probType: req.body.probType,
-            probDesc: req.body.probDesc,
-            emotion: emotionScore,
-            status: req.body.status,
-            victimName: req.body.victimName,
-            location: req.body.location,
-            contact: req.body.contact,
-            time: currentTime,
-            email: req.body.email
-        }).then(function (details) {
-            res.send(details);
-        }).catch(next);
-    }, 8000);
-});
 
-router.post('/addHep', async function (req, res, next) {
-    let documents = {
-        'documents': [
-            { 'id': '1', 'language': 'en', 'text': req.body.probDesc }
-        ]
-    }
-    var currentTime = Date();
-    currentTime = currentTime.toString().slice(4,24);
-
-    await get_sentiment(documents);
-    console.log("hii");
     Victim.create({
         probTitle: req.body.probTitle,
         probType: req.body.probType,
@@ -196,7 +129,6 @@ router.post('/addHep', async function (req, res, next) {
         res.send(details);
     }).catch(next);
 });
-
 
 //**********************DISASTER ALERTS**********************
 
@@ -407,5 +339,92 @@ router.get('/getAlerts/:lat/:lon',async function(req,res,next){
     res.send(resultFinal);
 });
   
+
+//*******************ALL-IN-ALL ALAGHU RAJA 2******************
+
+router.get('/getAllAlerts/:lat/:lon',async function(req,res,next){
+    var la = req.params.lat;
+    var lo = req.params.lon;
+    await Promise.all([disasters(),news(),weathers(la,lo)]);
+    console.log("Final");
+    res.send(resultFinal);
+});
+
+//***************DISASTER*****************
+async function disasters(){
+
+    var currentDate = new Date();
+    currentDate = currentDate.toISOString().slice(0,10);
+    instanceDisaster.defaults.headers.common['Authorization'] = 'Bearer 7XvdQkOUUrShrqq3QrHCYmwfJod5Oy';
+    //instanceDisaster.get('/events/?limit=5&category=disasters, severe-weather, terror&label=air-quality, biological-hazard, cold-wave, disaster, disaster-warning, drought, dust, earthquake, environment-pollution, epidemic, explosion, fire, heat-wave, hostage-crisis, hurricane, mass-shooting, nuclear, suspected-bombing, suspected-attack, technological-disaster, terror, tornado, tsunami, typhoon, vehicle-accident, volcano, weather, weather-warning, wildfire&country=US&active.gte='+currentDate+'&/')
+    await instanceDisaster.get('/events/?limit=10&category=disasters, severe-weather, terror&country=US&active.gte='+currentDate+'&/').then(function(response){
+        var result = response.data.results;
+        var newResult = [];      
+        result.forEach(element => {
+            var tempObj={};
+            tempObj.title=element.title;
+            tempObj.description=element.description;
+            tempObj.start=element.start;
+            tempObj.end=element.end;
+            tempObj.updated=element.updated;
+            newResult.push(tempObj);
+        });
+        resultFinal.disaster = newResult;
+        console.log("Disaster");
+    }).catch(function(err){
+        if(err.response){
+            console.log(err.response.data);
+            res.send("Unable to retrieve disaster alerts");
+        }
+    });
+}
+//****************NEWS*****************
+async function news(){
+    var body = {
+        "filter":
+            {
+            "field": "country",
+            "value": ["India"],
+            "operator": "AND"
+            }
+    };
+    await instanceNews.post('/reports?appname=disassister',body).then(function(response){
+        var result = response.data.data;
+        var newResult = [];
+        
+        result.forEach(element => {
+            var tempObj={};
+            tempObj.id=element.id;
+            tempObj.title=element.fields.title;
+            newResult.push(tempObj);
+        });
+        resultFinal.news = newResult;
+        console.log("News");
+    }).catch(function(err){
+        if(err.response){
+            console.log(err.response.data);
+            res.send("Unable to retrieve news");
+        }
+    });  
+}
+//*******************WEATHER*******************
+
+async function weathers(la,lo){
+    await instanceWeather.get('/2.5/weather?lat='+la+'&lon='+lo+'&appid=979e544ab2d464b05a32114170a8540f&units=metric').then(function(response){
+        var result = response.data;
+        var newResult = {};
+        newResult.description = result.weather[0].description;
+        newResult.temp = result.main.temp;
+        newResult.humidity = result.main.humidity;
+        newResult.windSpeed = result.wind.speed;
+        resultFinal.weather = newResult;
+        console.log("Weather");
+    }).catch(function(err){
+        if(err.response){
+            console.log(err.response.data);
+            res.send("Unable to retrieve weather alerts");
+        }
+    }); 
+}
 
 module.exports = router;
